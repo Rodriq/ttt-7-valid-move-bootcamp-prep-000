1933e7ea211bd4dd9fa3d5317a13d140bb7ccd47
Fogang Rodriq <fogangrodriq@gmail.com>
2018-09-08 03:21:17 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,11 +3,11 @@
 def valid_move?(board, index)
   # m = index-1.between?(1, 8)
   m = index-1
-  if !position_taken?(board, index)
-    true
-    elsif position_taken?(board, index)
-    false
-    elsif !index.between(1, 9)
+  # if !position_taken?(board, index)
+  #   true
+  #   elsif position_taken?(board, index)
+  #   false
+    if !index.between(1, 9)
     false
   end
 end
